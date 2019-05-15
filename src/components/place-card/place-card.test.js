6542f7922d24380bd4ce39b2279ef70e3fe9fe23
demikhovr <demikhovr@gmail.com>
2019-05-16 00:12:00 +0300

    refactor snapshot tests for PlaceCard component

--- src/components/place-card/place-card.test.js
+++ src/components/place-card/place-card.test.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import PlaceCard from './place-card.jsx';
-import offersMock from '../../mocks/offers';
+
+const mock = {
+  offer: {
+    id: 1,
+    mark: `Premium`,
+    imageSrc: `img/apartment-01.jpg`,
+    price: 120,
+    inBookmarks: true,
+    rating: 4,
+    title: `Beautiful luxurious apartment at great location`,
+    type: `Apartment`,
+  },
+};
 
 it(`PlaceCard correctly renders`, () => {
-  const [offer] = offersMock;
+  const {offer} = mock;
   const clickHandler = jest.fn();
   const tree = renderer
     .create(<PlaceCard
-      mark={offer.mark}
-      imageSrc={offer.imageSrc}
-      price={offer.price}
-      inBookmarks={offer.inBookmarks}
-      rating={offer.rating}
-      name={offer.name}
-      type={offer.type}
+      offer={offer}
       onTitleClick={clickHandler}
+      onImageClick={clickHandler}
     />)
     .toJSON();
 
