1952de12f7ef5b8a4e3f39832ed7433302b68fbe
demikhovr <demikhovr@gmail.com>
2019-05-19 20:12:15 +0300

    add cityCoordinates props and cityCoordinates and coordinates propTypes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 import offersMock from './mocks/offers';
 
+const cityCoordinates = [52.38333, 4.9];
+
 ReactDOM.render(
-    <App offers={offersMock} />,
+    <App
+      cityCoordinates={cityCoordinates}
+      offers={offersMock}
+    />,
     document.querySelector(`#root`)
 );
