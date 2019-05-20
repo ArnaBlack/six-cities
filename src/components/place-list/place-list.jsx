12323378fed8c3bdd8aa65ab13bbc7c4a78c539e
demikhovr <demikhovr@gmail.com>
2019-05-20 07:57:30 +0300

    fix key prop

--- src/components/place-list/place-list.jsx
+++ src/components/place-list/place-list.jsx
@@ -10,7 +10,7 @@ const PlaceList = (props) => {
 
   return <div className="cities__places-list places__list tabs__content">
     {offers.map((offer) => <PlaceCard
-      key={`offer-${offer.id}`}
+      key={offer.id}
       offer={offer}
       onImageClick={onSelectOffer}
     />)}
