cc7c9c874b342c121f56b164590559ef90091fb3
demikhovr <demikhovr@gmail.com>
2019-05-25 22:06:15 +0300

    init store and wrap App component in Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
+import reducer from './store/reducers/reducer';
 import offersMock from './mocks/offers';
+
+const store = createStore(reducer);
 
 const cityCoordinates = [52.38333, 4.9];
 
 ReactDOM.render(
-    <App
-      cityCoordinates={cityCoordinates}
-      offers={offersMock}
-    />,
+    <Provider store={store}>
+      <App
+        cityCoordinates={cityCoordinates}
+        offers={offersMock}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
