3bf293b0c3b53c6d6c0648f0083a02c28bd85d9c
demikhovr <demikhovr@gmail.com>
2019-05-25 22:27:22 +0300

    update reducer: add CHANGE_CITY and GET_OFFERS actions, tests for reducer

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,3 +1,8 @@
+import {
+  CHANGE_CITY,
+  GET_OFFERS,
+} from '../actions/action-types';
+
 const initialState = {
   city: `Amsterdam`,
   offers: [],
@@ -5,6 +10,16 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case CHANGE_CITY:
+      return {
+        ...state,
+        city: action.city,
+      };
+    case GET_OFFERS:
+      return {
+        ...state,
+        offers: action.offers,
+      };
     default:
       return state;
   }
