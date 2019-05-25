ce199ec3696a4b05b45c22974f0976e2e40611cb
demikhovr <demikhovr@gmail.com>
2019-05-25 21:22:36 +0300

    add reducer

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  city: `Amsterdam`,
+  offers: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
