4dcead04fc1d80b55a8857bca59e4ce3d0331cf5
demikhovr <demikhovr@gmail.com>
2019-05-26 19:24:11 +0300

    add Cities component with tests

---
+++ src/components/cities/cities.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import CityLink from '../city-link/city-link.jsx';
+
+const Cities = ({currentCity, cities}) => <section className="locations container">
+  <ul className="locations__list tabs__list">
+    {cities.map((it, i) => <li
+      key={`city-${i}`}
+      className="locations__item"
+    >
+      <CityLink
+        city={it}
+        isActive={it === currentCity}
+      />
+    </li>)}
+  </ul>
+</section>;
+
+Cities.propTypes = {
+  currentCity: PropTypes.string.isRequired,
+  cities: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const mapStateToProps = (state, props) => ({
+  ...props,
+  currentCity: state.city,
+  cities: [...new Set(state.offers.map((it) => it.city))],
+});
+
+export {Cities};
+export default connect(mapStateToProps)(Cities);
---
+++ src/components/cities/cities.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallowToJson} from 'enzyme-to-json';
+import {Cities} from './cities.jsx';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const mock = {
+  currentCity: `Amsterdam`,
+  cities: [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`],
+};
+
+it(`Cities renders correctly`, () => {
+  const {
+    currentCity,
+    cities,
+  } = mock;
+  const tree = shallow(<Cities
+    currentCity={currentCity}
+    cities={cities}
+  />);
+
+  expect(shallowToJson(tree)).toMatchSnapshot();
+});
+
