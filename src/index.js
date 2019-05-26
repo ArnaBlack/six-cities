edd6e99c706eae908d62481a30bdbdfa707b50ec
demikhovr <demikhovr@gmail.com>
2019-05-26 18:40:16 +0300

    refactor index.js: remove props from app

--- src/index.js
+++ src/index.js
@@ -4,18 +4,12 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 import reducer from './store/reducers/reducer';
-import offersMock from './mocks/offers';
 
 const store = createStore(reducer);
 
-const cityCoordinates = [52.38333, 4.9];
-
 ReactDOM.render(
     <Provider store={store}>
-      <App
-        cityCoordinates={cityCoordinates}
-        offers={offersMock}
-      />
+      <App />
     </Provider>,
     document.querySelector(`#root`)
 );
