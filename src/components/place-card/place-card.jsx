662fa0d45fa54ad0fdd434d6150866cbb2753571
demikhovr <demikhovr@gmail.com>
2019-05-26 19:14:33 +0300

    refactor PlaceCard component

--- src/components/place-card/place-card.jsx
+++ src/components/place-card/place-card.jsx
@@ -22,11 +22,7 @@ class PlaceCard extends PureComponent {
       rating,
     } = this.props.offer;
     const ratingWidth = Math.round(rating) * 100 / MAX_RATING;
-    const bookMarkBtnClasses = [`place-card__bookmark-button`, `button`];
-
-    if (inBookmarks) {
-      bookMarkBtnClasses.push(`place-card__bookmark-button--active`);
-    }
+    const bookmarkActiveClass = inBookmarks ? `place-card__bookmark-button--active` : ``;
 
     return <article className="cities__place-card place-card">
       {mark ? (
@@ -45,7 +41,10 @@ class PlaceCard extends PureComponent {
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className={bookMarkBtnClasses.join(` `)} type="button">
+          <button
+            className={`place-card__bookmark-button button ${bookmarkActiveClass}`}
+            type="button"
+          >
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"/>
             </svg>
