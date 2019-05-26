59c6ae37b22d2398f3bccf72935c4b4893d6ab7b
demikhovr <demikhovr@gmail.com>
2019-05-26 18:27:48 +0300

    add cities mock

---
+++ src/mocks/cities.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: `Paris`,
+    coordinates: [48.85341, 2.3488],
+  },
+  {
+    name: `Cologne`,
+    coordinates: [50.941357, 6.958307],
+  },
+  {
+    name: `Brussels`,
+    coordinates: [50.8505, 4.3488],
+  },
+  {
+    name: `Amsterdam`,
+    coordinates: [52.38333, 4.9],
+  },
+  {
+    name: `Hamburg`,
+    coordinates: [53.551086, 9.993682],
+  },
+  {
+    name: `Dusseldorf`,
+    coordinates: [51.217941, 6.761680],
+  },
+];
