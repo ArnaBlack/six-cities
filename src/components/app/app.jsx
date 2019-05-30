6d1f9e215cdb7140b477bd3b03c5974af8e2fdb7
demikhovr <demikhovr@gmail.com>
2019-05-30 07:11:15 +0300

    refactor App component: remove redux, remove props, update tests

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,57 +1,10 @@
 import React, {PureComponent} from 'react';
 import Main from '../main/main.jsx';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import ActionCreators from '../../store/actions/action-creators';
 
 class App extends PureComponent {
   render() {
-    const {
-      currentCity,
-      offers,
-    } = this.props;
-
-    return <Main
-      currentCity={currentCity}
-      offers={offers}
-    />;
-  }
-
-  componentDidMount() {
-    const {getOffers} = this.props;
-    getOffers();
+    return <Main />;
   }
 }
 
-App.propTypes = {
-  currentCity: PropTypes.string.isRequired,
-  offers: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    mark: PropTypes.string,
-    imageSrc: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    inBookmarks: PropTypes.bool.isRequired,
-    rating: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    coordinates: PropTypes.arrayOf(PropTypes.number),
-  })),
-  getOffers: PropTypes.func.isRequired,
-};
-
-App.defaultProps = {
-  offers: [],
-};
-
-const mapStateToProps = (state, props) => ({
-  ...props,
-  currentCity: state.city,
-  offers: state.offers.filter((it) => it.city === state.city),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  getOffers: () => dispatch(ActionCreators.getOffers())
-});
-
-export {App};
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
