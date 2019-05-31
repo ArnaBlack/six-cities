dc6b0bf04bde42bc8c0e6a4a7422769b0f9e6f3b
demikhovr <demikhovr@gmail.com>
2019-05-31 22:53:51 +0300

    refactor App component: wrap Main in withActiveItem hoc, update tests

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,18 @@
 import React, {PureComponent} from 'react';
 import Main from '../main/main.jsx';
+import withActiveItem from '../../hocs/with-active-item/with-actvie-item.jsx';
+import withTransformProps from '../../hocs/with-transform-props/with-transform-props.jsx';
+
+const transformSelectedToActive = (props) => ({
+  selectedOffer: props.activeItem,
+  onSelectOffer: props.onSelectItem,
+});
+
+const MainWrapped = withActiveItem(withTransformProps(transformSelectedToActive)(Main));
 
 class App extends PureComponent {
   render() {
-    return <Main />;
+    return <MainWrapped />;
   }
 }
 
