5f8a8d79aa03309fff06061ce3d30fea947ee962
demikhovr <demikhovr@gmail.com>
2019-06-01 18:45:56 +0300

    refactor api.js

--- src/api.js
+++ src/api.js
@@ -3,12 +3,17 @@ import axios from 'axios';
 const BASE_URL = `https://es31-server.appspot.com/six-cities`
 const TIMEOUT = 5000;
 
-const api = axios.create({
-  baseURL: BASE_URL,
-  timeout: TIMEOUT,
-  withCredentials: true,
-});
+export const createAPI = () => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: TIMEOUT,
+    withCredentials: true,
+  });
 
-export default () => {
+  const onSuccess = (response) => response;
+  const onFail = (err) => err;
+
+  api.interceptors.response.use(onSuccess, onFail);
+
   return api;
 };
