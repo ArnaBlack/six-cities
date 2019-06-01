7319538b07c0714e7aaf64fc5300dbfc45bd7e74
demikhovr <demikhovr@gmail.com>
2019-06-01 22:23:46 +0300

    refactor index.js: add thunk with api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import App from './components/app/app.jsx';
+import thunk from 'redux-thunk';
+import {compose} from 'recompose';
+
 import reducer from './store/reducers/reducer';
+import {createAPI} from './api';
+import App from './components/app/app.jsx';
+
+const api = createAPI((...args) => store.dispatch(...args));
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ),
 );
 
 ReactDOM.render(
