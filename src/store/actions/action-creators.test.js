38bfa52839380fed955e6d1b2ec961b691760bfb
demikhovr <demikhovr@gmail.com>
2019-06-02 20:39:34 +0300

    update tests for ActionCreators and reducer

--- src/store/actions/action-creators.test.js
+++ src/store/actions/action-creators.test.js
@@ -1,6 +1,6 @@
 import {
   CHANGE_CITY,
-  GET_OFFERS,
+  LOAD_OFFERS,
 } from './action-types';
 import ActionCreators from './action-creators';
 
@@ -9,7 +9,7 @@ describe(`Action creators work correctly`, () => {
     const city = `Paris`;
     const expected = {
       type: CHANGE_CITY,
-      city,
+      payload: city,
     };
 
     expect(ActionCreators.changeCity(city)).toEqual(expected);
@@ -18,113 +18,47 @@ describe(`Action creators work correctly`, () => {
   it(`Action creator for getting offers returns correct action`, () => {
     const offers = [
       {
+        bedrooms: 1,
+        city: {
+          name: `Brussels`,
+          location: {
+            latitude: 50.846557,
+            longitude: 4.351697,
+            zoom: 13,
+          }
+        },
+        description: `Description`,
+        goods: [`Washer`, `Towels`],
+        host: {
+          avatarUrl: `img/avatar-angelina.jpg`,
+          id: 25,
+          isPro: true,
+          name: `Name`,
+        },
         id: 1,
-        city: `Amsterdam`,
-        mark: `Premium`,
-        imageSrc: `img/apartment-01.jpg`,
-        price: 120,
-        inBookmarks: true,
-        rating: 4,
-        title: `Beautiful luxurious apartment at great location`,
-        type: `Apartment`,
-        coordinates: [52.3909553943508, 4.85309666406198],
-      },
-      {
-        id: 2,
-        city: `Amsterdam`,
-        imageSrc: `img/room.jpg`,
-        price: 80,
-        inBookmarks: false,
-        rating: 4.5,
-        title: `Wood and stone place`,
-        type: `Private room`,
-        coordinates: [52.369553943508, 4.85309666406198],
-      },
-      {
-        id: 3,
-        city: `Amsterdam`,
-        imageSrc: `img/apartment-02.jpg`,
-        price: 132,
-        inBookmarks: true,
-        rating: 2.3,
-        title: `Canal View Prinsengracht`,
-        type: `Apartment`,
-        coordinates: [52.3909553943508, 4.929309666406198],
-      },
-      {
-        id: 4,
-        city: `Amsterdam`,
-        mark: `Premium`,
-        imageSrc: `img/apartment-03.jpg`,
-        price: 180,
-        inBookmarks: false,
-        rating: 3.1,
-        title: `Nice, cozy, warm big bed apartment`,
-        type: `Apartment`,
-        coordinates: [52.3809553943508, 4.939309666406198],
-      },
-      {
-        id: 5,
-        city: `Paris`,
-        mark: `Premium`,
-        imageSrc: `img/apartment-01.jpg`,
-        price: 100,
-        inBookmarks: false,
-        rating: 3.1,
-        title: `Nice, cozy, warm big bed apartment`,
-        type: `Apartment`,
-        coordinates: [48.85341, 2.3488],
-      },
-      {
-        id: 6,
-        city: `Cologne`,
-        imageSrc: `img/apartment-01.jpg`,
-        price: 100,
-        inBookmarks: false,
-        rating: 3.1,
-        title: `Nice, cozy, warm big bed apartment`,
-        type: `Apartment`,
-        coordinates: [50.941357, 6.958307],
-      },
-      {
-        id: 7,
-        city: `Brussels`,
-        imageSrc: `img/apartment-01.jpg`,
-        price: 100,
-        inBookmarks: false,
-        rating: 3.1,
-        title: `Nice, cozy, warm big bed apartment`,
-        type: `Apartment`,
-        coordinates: [50.8505, 4.3488],
-      },
-      {
-        id: 8,
-        city: `Hamburg`,
-        imageSrc: `img/apartment-01.jpg`,
-        price: 100,
-        inBookmarks: false,
-        rating: 3.1,
-        title: `Nice, cozy, warm big bed apartment`,
-        type: `Apartment`,
-        coordinates: [53.551086, 9.993682],
-      },
-      {
-        id: 9,
-        city: `Dusseldorf`,
-        imageSrc: `img/apartment-01.jpg`,
-        price: 100,
-        inBookmarks: false,
-        rating: 3.1,
-        title: `Nice, cozy, warm big bed apartment`,
-        type: `Apartment`,
-        coordinates: [51.217941, 6.761680],
+        images: [`https://es31-server.appspot.com/six-cities/static/hotel/19.jpg`],
+        isFavorite: true,
+        isPremium: false,
+        location: {
+          latitude: 50.846557,
+          longitude: 4.351697,
+          zoom: 13,
+        },
+        maxAdults: 3,
+        previewImage: `https://es31-server.appspot.com/six-cities/static/hotel/4.jpg`,
+        price: 117,
+        rating: 3.4,
+        title: `The house among olive`,
+        type: `room`,
       },
     ];
+
     const expected = {
-      type: GET_OFFERS,
-      offers,
+      type: LOAD_OFFERS,
+      payload: offers,
+      isLoading: false,
     };
 
-    expect(ActionCreators.getOffers()).toEqual(expected);
+    expect(ActionCreators.loadOffers(offers)).toEqual(expected);
   });
 });
--- src/store/reducers/reducer.test.js
+++ src/store/reducers/reducer.test.js
@@ -1,6 +1,6 @@
 import {
   CHANGE_CITY,
-  GET_OFFERS,
+  LOAD_OFFERS,
 } from '../actions/action-types';
 import reducer from './reducer';
 
@@ -12,7 +12,7 @@ it(`Should change city`, () => {
 
   const action = {
     type: CHANGE_CITY,
-    city: `Paris`,
+    payload: `Paris`,
   };
 
   const expected = {
@@ -26,66 +26,56 @@ it(`Should change city`, () => {
 it(`Should get offers`, () => {
   const offers = [
     {
+      bedrooms: 1,
+      city: {
+        name: `Brussels`,
+        location: {
+          latitude: 50.846557,
+          longitude: 4.351697,
+          zoom: 13,
+        }
+      },
+      description: `Description`,
+      goods: [`Washer`, `Towels`],
+      host: {
+        avatarUrl: `img/avatar-angelina.jpg`,
+        id: 25,
+        isPro: true,
+        name: `Name`,
+      },
       id: 1,
-      city: `Amsterdam`,
-      mark: `Premium`,
-      imageSrc: `img/apartment-01.jpg`,
-      price: 120,
-      inBookmarks: true,
-      rating: 4,
-      title: `Beautiful luxurious apartment at great location`,
-      type: `Apartment`,
-      coordinates: [52.3909553943508, 4.85309666406198],
-    },
-    {
-      id: 5,
-      city: `Paris`,
-      mark: `Premium`,
-      imageSrc: `img/apartment-01.jpg`,
-      price: 100,
-      inBookmarks: false,
-      rating: 3.1,
-      title: `Nice, cozy, warm big bed apartment`,
-      type: `Apartment`,
-      coordinates: [48.85341, 2.3488],
-    },
-    {
-      id: 7,
-      city: `Brussels`,
-      imageSrc: `img/apartment-01.jpg`,
-      price: 100,
-      inBookmarks: false,
-      rating: 3.1,
-      title: `Nice, cozy, warm big bed apartment`,
-      type: `Apartment`,
-      coordinates: [50.8505, 4.3488],
-    },
-    {
-      id: 8,
-      city: `Hamburg`,
-      imageSrc: `img/apartment-01.jpg`,
-      price: 100,
-      inBookmarks: false,
-      rating: 3.1,
-      title: `Nice, cozy, warm big bed apartment`,
-      type: `Apartment`,
-      coordinates: [53.551086, 9.993682],
+      images: [`https://es31-server.appspot.com/six-cities/static/hotel/19.jpg`],
+      isFavorite: true,
+      isPremium: false,
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13,
+      },
+      maxAdults: 3,
+      previewImage: `https://es31-server.appspot.com/six-cities/static/hotel/4.jpg`,
+      price: 117,
+      rating: 3.4,
+      title: `The house among olive`,
+      type: `room`,
     },
   ];
 
   const state = {
     city: `Amsterdam`,
     offers: [],
+    isLoading: true,
   };
 
   const action = {
-    type: GET_OFFERS,
-    offers,
+    type: LOAD_OFFERS,
+    payload: offers,
   };
 
   const expected = {
     city: `Amsterdam`,
     offers,
+    isLoading: false,
   };
 
   expect(reducer(state, action)).toEqual(expected);
