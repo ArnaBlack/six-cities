a98c3f21937272f97488d03863fce3004a00507e
demikhovr <demikhovr@gmail.com>
2019-06-02 21:12:15 +0300

    refactor Main component: functional component => class component, update propTypes and props

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -6,19 +6,48 @@ import {Main} from './main.jsx';
 Enzyme.configure({adapter: new Adapter()});
 
 const mock = {
-  currentCity: `Amsterdam`,
+  currentCity: {
+    name: `Brussels`,
+    location: {
+      latitude: 50.846557,
+      longitude: 4.351697,
+      zoom: 13,
+    },
+  },
   offers: [
     {
+      bedrooms: 1,
+      city: {
+        name: `Brussels`,
+        location: {
+          latitude: 50.846557,
+          longitude: 4.351697,
+          zoom: 13,
+        },
+      },
+      description: `Description`,
+      goods: [`Washer`, `Towels`],
+      host: {
+        avatarUrl: `img/avatar-angelina.jpg`,
+        id: 25,
+        isPro: true,
+        name: `Name`,
+      },
       id: 1,
-      city: `Amsterdam`,
-      mark: `Premium`,
-      imageSrc: `img/apartment-01.jpg`,
-      price: 120,
-      inBookmarks: true,
-      rating: 4,
-      title: `Beautiful luxurious apartment at great location`,
-      type: `Apartment`,
-      coordinates: [52.3909553943508, 4.85309666406198],
+      images: [`https://es31-server.appspot.com/six-cities/static/hotel/19.jpg`],
+      isFavorite: true,
+      isPremium: false,
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13,
+      },
+      maxAdults: 3,
+      previewImage: `https://es31-server.appspot.com/six-cities/static/hotel/4.jpg`,
+      price: 117,
+      rating: 3.4,
+      title: `The house among olive`,
+      type: `room`,
     },
   ],
 };
