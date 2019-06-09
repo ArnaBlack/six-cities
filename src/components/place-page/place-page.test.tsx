bf561cdc78240914e791c0e1c01de60cd1400cac
demikhovr <demikhovr@gmail.com>
2019-06-09 21:03:31 +0300

    add PlacePage component with tests

---
+++ src/components/place-page/place-page.test.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import {
+  configure,
+  shallow,
+} from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import {PlacePage} from './place-page';
+
+configure({adapter: new Adapter()});
+
+const mock = {
+  isLoading: false,
+  offer: {
+    bedrooms: 1,
+    city: {
+      name: `Brussels`,
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13,
+      },
+    },
+    description: `Description`,
+    goods: [`Washer`, `Towels`],
+    host: {
+      avatarUrl: `img/avatar-angelina.jpg`,
+      id: 25,
+      isPro: true,
+      name: `Name`,
+    },
+    id: 1,
+    images: [`https://es31-server.appspot.com/six-cities/static/hotel/19.jpg`],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 50.846557,
+      longitude: 4.351697,
+      zoom: 13,
+    },
+    maxAdults: 3,
+    previewImage: `https://es31-server.appspot.com/six-cities/static/hotel/4.jpg`,
+    price: 117,
+    rating: 3.4,
+    title: `The house among olive`,
+    type: `room`,
+  },
+};
+
+it(`PlacePage correctly renders`, () => {
+  const {
+    isLoading,
+    offer,
+  } = mock;
+  const checkAuth = jest.fn();
+  const loadOffers = jest.fn();
+  const tree = shallow(<PlacePage
+    isLoading={isLoading}
+    offer={offer}
+    checkAuth={checkAuth}
+    loadOffers={loadOffers}
+  />);
+
+  expect(tree).toMatchSnapshot();
+});
