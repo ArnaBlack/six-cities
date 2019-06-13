fecf179979fd9ad4c7503ae4cd28624d6775a0c4
demikhovr <demikhovr@gmail.com>
2019-06-13 20:53:44 +0300

    remove withPageWrapper

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -9,7 +9,6 @@ import {
 import withActiveItem from '../../hocs/with-active-item/with-active-item';
 import withTransformProps from '../../hocs/with-transform-props/with-transform-props';
 import withPrivateRoute from '../../hocs/with-private-route/with-private-route';
-import withPageWrapper from '../../hocs/with-page-wrapper/with-page-wrapper';
 
 import Loader from '../loader/loader';
 import MainPage from '../main-page/main-page';
@@ -28,17 +27,8 @@ const transformActiveToSelected = (props) => ({
   selectedOffer: props.activeItem,
   onSelectOffer: props.onSelectItem,
 });
-
-const MainPageWrapped = withPageWrapper(
-  withActiveItem(
-    withTransformProps(
-      transformActiveToSelected)(MainPage)
-  ), `page page--gray page--main`
-);
-const SignInPageWrapped = withPageWrapper(SignInPage, `page page--gray page--login`);
-const FavoritesPageWrapped = withPageWrapper(
-  withPrivateRoute(FavoritesPage), `page`
-);
+const MainPageWrapped = withActiveItem(withTransformProps(transformActiveToSelected)(MainPage));
+const FavoritesPageWrapped = withPrivateRoute(FavoritesPage);
 
 interface Props {
   isLoading: boolean,
@@ -57,7 +47,7 @@ class App extends React.PureComponent<Props, null> {
 
     return isLoading ? <Loader /> : <Switch>
       <Route path="/" exact component={MainPageWrapped} />
-      <Route path="/login" render={() => isAuthorizationRequired ? <SignInPageWrapped /> : <Redirect to="/" />}/>
+      <Route path="/login" render={() => isAuthorizationRequired ? <SignInPage /> : <Redirect to="/" />}/>
       <Route path="/favorites" component={FavoritesPageWrapped} />
       <Route path="/offer/:id" component={PlacePage} />
       <Redirect from="*" to="/" />
