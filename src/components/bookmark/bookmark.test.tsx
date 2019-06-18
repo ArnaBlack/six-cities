9889bd02b4a28a38ccd1a1f8177e96e7d4347157
demikhovr <demikhovr@gmail.com>
2019-06-18 19:54:28 +0300

    add Bookmark component with tests

---
+++ src/components/bookmark/bookmark.test.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import {Bookmark} from './bookmark';
+
+const mock = {
+  id: 1,
+  isFavorite: false,
+  className: ``,
+  width: 18,
+  height: 19,
+};
+
+it(`Bookmark renders correctly`, () => {
+  const {
+    id,
+    isFavorite,
+    className,
+    width,
+    height,
+  } = mock;
+  const onClick = jest.fn();
+  const tree = renderer
+    .create(<Bookmark
+      id={id}
+      isFavorite={isFavorite}
+      className={className}
+      width={width}
+      height={height}
+      onClick={onClick}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
